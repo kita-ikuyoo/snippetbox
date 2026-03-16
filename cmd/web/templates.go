e9f230da44fe7cad2e32a06a8db77e051de425e3
喜多郁代 <79817738+kita-ikuyoo@users.noreply.github.com>
2026-03-16 17:03:42 +0900

    Transform custom functions in home.html and view.html to pipeing fashion

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -30,43 +30,6 @@ var functions = template.FuncMap{
 	"humanDate": humanDate,
 }
 
-//	func newTemplateCache() (map[string]*template.Template, error) {
-//		// Initialize a new map to act as the cache.
-//		cache := map[string]*template.Template{}
-//		// Use the filepath.Glob() function to get a slice of all filepaths that
-//		// match the pattern "./ui/html/pages/*.tmpl". This will essentially give
-//		// us a slice of all the filepaths for our application 'page' templates
-//		// like [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
-//		pages, err := filepath.Glob("./ui/html/pages/*.html")
-//		if err != nil {
-//			return nil, err
-//		}
-//		// Loop through the page filepaths one-by-one.
-//		for _, page := range pages {
-//			// Extract the file name (like 'home.tmpl') from the full filepath
-//			// and assign it to the name variable.
-//			name := filepath.Base(page)
-//			// Parse the files into a template set.
-//			ts, err := template.ParseFiles("./ui/html/base.html")
-//			if err != nil {
-//				return nil, err
-//			}
-//			// We can call ParseGlob or ParseFiles on *Template to add the page template
-//			ts, err = ts.ParseGlob("./ui/html/partials/*.html")
-//			if err != nil {
-//				return nil, err
-//			}
-//			ts, err = ts.ParseFiles(page)
-//			if err != nil {
-//				return nil, err
-//			}
-//			// Add the template set to the map, using the name of the page
-//			// (like 'home.tmpl') as the key.
-//			cache[name] = ts
-//		}
-//		// Return the map.
-//		return cache, nil
-//	}
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
