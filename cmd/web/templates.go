1fe54442811bc25520d6a91d3ee26d2a4253e412
喜多郁代 <79817738+kita-ikuyoo@users.noreply.github.com>
2026-03-16 16:59:51 +0900

    Add Created and Expired to base.html and home.html

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"path/filepath"
 	"snippetbox/internal/models"
+	"time"
 )
 
 // Define a templateData type to act as the holding structure for
@@ -16,28 +17,73 @@ type templateData struct {
 	Snippets    []models.Snippet
 }
 
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time value.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap value and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup table mapping names to
+// functions.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+//	func newTemplateCache() (map[string]*template.Template, error) {
+//		// Initialize a new map to act as the cache.
+//		cache := map[string]*template.Template{}
+//		// Use the filepath.Glob() function to get a slice of all filepaths that
+//		// match the pattern "./ui/html/pages/*.tmpl". This will essentially give
+//		// us a slice of all the filepaths for our application 'page' templates
+//		// like [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
+//		pages, err := filepath.Glob("./ui/html/pages/*.html")
+//		if err != nil {
+//			return nil, err
+//		}
+//		// Loop through the page filepaths one-by-one.
+//		for _, page := range pages {
+//			// Extract the file name (like 'home.tmpl') from the full filepath
+//			// and assign it to the name variable.
+//			name := filepath.Base(page)
+//			// Parse the files into a template set.
+//			ts, err := template.ParseFiles("./ui/html/base.html")
+//			if err != nil {
+//				return nil, err
+//			}
+//			// We can call ParseGlob or ParseFiles on *Template to add the page template
+//			ts, err = ts.ParseGlob("./ui/html/partials/*.html")
+//			if err != nil {
+//				return nil, err
+//			}
+//			ts, err = ts.ParseFiles(page)
+//			if err != nil {
+//				return nil, err
+//			}
+//			// Add the template set to the map, using the name of the page
+//			// (like 'home.tmpl') as the key.
+//			cache[name] = ts
+//		}
+//		// Return the map.
+//		return cache, nil
+//	}
 func newTemplateCache() (map[string]*template.Template, error) {
-	// Initialize a new map to act as the cache.
 	cache := map[string]*template.Template{}
-	// Use the filepath.Glob() function to get a slice of all filepaths that
-	// match the pattern "./ui/html/pages/*.tmpl". This will essentially give
-	// us a slice of all the filepaths for our application 'page' templates
-	// like [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
-	// Loop through the page filepaths one-by-one.
 	for _, page := range pages {
-		// Extract the file name (like 'home.tmpl') from the full filepath
-		// and assign it to the name variable.
 		name := filepath.Base(page)
-		// Parse the files into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New("base.html").Funcs(functions).ParseFiles("./ui/html/base.html")
+
 		if err != nil {
 			return nil, err
 		}
-		// We can call ParseGlob or ParseFiles on *Template to add the page template
 		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
 		if err != nil {
 			return nil, err
@@ -46,10 +92,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		if err != nil {
 			return nil, err
 		}
-		// Add the template set to the map, using the name of the page
-		// (like 'home.tmpl') as the key.
 		cache[name] = ts
 	}
-	// Return the map.
 	return cache, nil
 }
