1bcfa5c5ea0c5ee80e5708efbee337aab7ef8563
喜多郁代 <79817738+kita-ikuyoo@users.noreply.github.com>
2026-03-17 13:22:47 +0900

    Add github.com/justinas/alice to make simplify the chain of handler middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/justinas/alice"
 	"net/http"
 )
 
@@ -13,6 +14,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	return standard.Then(mux)
 
 }
