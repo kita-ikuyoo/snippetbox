3aace34ff1e3f80ebd36ae53ac6dfe5fb9ce24c0
喜多郁代 <79817738+kita-ikuyoo@users.noreply.github.com>
2026-03-17 15:06:21 +0900

    Repopulate form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,6 +10,13 @@ import (
 	"unicode/utf8"
 )
 
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -56,30 +63,36 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	// Use the r.PostForm.Get() method to retrieve the title and content
-	// from the r.PostForm map.
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
+
 	// The r.PostForm.Get() method always returns the form data as a *string*.
 	// However, we're expecting our expires value to be a number, and want to
 	// represent it in our Go code as an integer. So we need to manually convert
 	// the form data to an integer using strconv.Atoi(), and send a 400 Bad
 	// Request response if the conversion fails.
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
 	// Initialize a map to hold any validation errors for the form fields.
 	fieldErrors := make(map[string]string)
 	// Check that the title value is not blank and is not more than 100
 	// characters long. If it fails either of those checks, add a message to the
 	// errors map using the field name as the key.
-	if strings.TrimSpace(title) == "" {
+	if strings.TrimSpace(form.Title) == "" {
 		fieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
+	} else if utf8.RuneCountInString(form.Title) > 100 {
 		// len() counts bytes. If you input "世界", it contains 3 bytes.
 		fieldErrors["title"] = "This field cannot be more than 100 characters long"
 	}
 	// Check that the content value isn't blank.
-	if strings.TrimSpace(content) == "" {
+	if strings.TrimSpace(form.Content) == "" {
 		fieldErrors["content"] = "This field cannot be blank"
 	}
 	// Check the expires value matches one of the permitted values (1, 7 or
@@ -90,10 +103,12 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	// If there are any errors, dump them in a plain-text HTTP response and
 	// return from the handler.
 	if len(fieldErrors) > 0 {
-		fmt.Fprint(w, fieldErrors)
+		data := app.newTemplateData()
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.html", data)
 		return
 	}
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Title, form.Content, expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
